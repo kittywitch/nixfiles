e21ebb4d8847dbaecc345acafb874b41b4762a09
kat witch <kat@kittywit.ch>
2020-11-13 21:03:13 +0000

    Added OBS Studio to the gaming profile.

--- configuration/profiles/gaming/default.nix
+++ configuration/profiles/gaming/default.nix
@@ -3,5 +3,5 @@
   hardware.opengl.extraPackages32 = with pkgs.pkgsi686Linux; [ libva ];
   hardware.pulseaudio.support32Bit = true;
 
-  home-manager.users.kat = { home.packages = [ pkgs.steam pkgs.steam-run pkgs.multimc ]; };
+  home-manager.users.kat = { home.packages = [ pkgs.steam pkgs.steam-run pkgs.multimc pkgs.obs-studio ]; };
 }
