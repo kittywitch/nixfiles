a4442c204e10bd3fef1e3fb21176e581aa547710
kat witch <kat@kittywit.ch>
2020-11-28 01:44:26 +0000

    Added to-do Sway move-over. ._.

--- configuration/profiles/gaming/default.nix
+++ configuration/profiles/gaming/default.nix
@@ -3,5 +3,5 @@
   hardware.opengl.extraPackages32 = with pkgs.pkgsi686Linux; [ libva ];
   hardware.pulseaudio.support32Bit = true;
 
-  home-manager.users.kat = { home.packages = [ pkgs.steam pkgs.steam-run pkgs.multimc pkgs.obs-studio ]; };
+  home-manager.users.kat = { home.packages = [ pkgs.lutris pkgs.steam pkgs.steam-run pkgs.multimc pkgs.obs-studio ]; };
 }
