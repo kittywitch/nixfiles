dd8758c8bc3f96a632007133717a027da8b47b1e
kat witch <kat@kittywit.ch>
2021-03-14 04:20:06 +0000

    NERDTree productivity improvements

--- config/profiles/kat/home/vim/vimrc.nix
+++ config/profiles/kat/home/vim/vimrc.nix
@@ -18,6 +18,14 @@
 
   " Open NERDTree at start
   autocmd VimEnter * NERDTree | wincmd p
+  autocmd BufEnter * if tabpagenr('$') == 1 && winnr('$') == 1 && exists('b:NERDTree') && b:NERDTree.isTabTree() |
+      \ quit | endif
+  autocmd BufEnter * if bufname('#') =~ 'NERD_tree_\d\+' && bufname('%') !~ 'NERD_tree_\d\+' && winnr('$') > 1 |
+      \ let buf=bufnr() | buffer# | execute "normal! \<C-W>w" | execute 'buffer'.buf | endif
+  nnoremap <leader>n :NERDTreeFocus<CR>
+  nnoremap <C-n> :NERDTree<CR>
+  nnoremap <C-t> :NERDTreeToggle<CR>
+  nnoremap <C-f> :NERDTreeFind<CR>
 
   let g:coc_node_path='${pkgs.nodejs}/bin/node'
 
