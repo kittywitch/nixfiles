1ed4578b51156e8a29ec576b8dbb53e0815aeb27
kat witch <kat@kittywit.ch>
2021-03-14 02:57:26 +0000

    Git plugins, config changes

--- config/profiles/kat/home/git.nix
+++ config/profiles/kat/home/git.nix
@@ -2,14 +2,27 @@
 
 {
   config = lib.mkIf config.deploy.profile.kat {
-    home.packages = with pkgs; [ git-crypt gitAndTools.gitRemoteGcrypt ];
+    home.packages = with pkgs; [
+      git-crypt
+      gitAndTools.gitRemoteGcrypt
+      gitAndTools.gitAnnex
+      git-revise
+      arc.pkgs.gitAndTools.git-annex-remote-b2
+    ];
 
     programs.git = {
       enable = true;
       package = pkgs.gitAndTools.gitFull;
       userName = "kat witch";
       userEmail = "kat@kittywit.ch";
-      extraConfig = { protocol.gcrypt.allow = "always"; };
+      extraConfig = {
+        protocol.gcrypt.allow = "always";
+        annex = {
+          autocommit = false;
+          backend = "SHA256";
+          synccontent = true;
+        };
+      };
       signing = {
         key = "01F50A29D4AA91175A11BDB17248991EFA8EFBEE";
         signByDefault = true;
