a2f23a2db74bcd6e8eb81f68c0360256b0a97e2d
kat witch <kat@kittywit.ch>
2021-03-26 21:36:11 +0000

    profiles/gui: Added gstreamer packages

--- profiles/gui/home/packages.nix
+++ profiles/gui/home/packages.nix
@@ -22,6 +22,12 @@ in {
       exiftool
       audacity
       avidemux
+      gst_all_1.gstreamer.dev
+      gst_all_1.gstreamer
+      gst_all_1.gst-plugins-base 
+      gst_all_1.gst-plugins-good 
+      gst_all_1.gst-plugins-bad
+      gst_all_1.gst-plugins-ugly
       vlc
       ffmpeg-full
       thunderbird
