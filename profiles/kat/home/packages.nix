47116a0dc6c81eedf81f184a129e4001b5a42548
kat witch <kat@kittywit.ch>
2021-03-26 22:38:50 +0000

    profiles/home: Fancy packages

--- profiles/kat/home/packages.nix
+++ profiles/kat/home/packages.nix
@@ -2,6 +2,6 @@
 
 {
   config = lib.mkIf config.deploy.profile.kat {
-    home.packages = with pkgs; [ kitty.terminfo ];
+    home.packages = with pkgs; [ kitty.terminfo hyperfine hexyl tokei ];
   };
 }
