2b258b083f5a8915d88e872ccdb632ce9fd02747
kat witch <kat@kittywit.ch>
2021-04-04 22:20:08 +0100

    services/syncplay: Moved to sync.kittywit.ch

--- services/syncplay.nix
+++ services/syncplay.nix
@@ -3,14 +3,26 @@
 {
   users.users.syncplay = { isSystemUser = true; };
 
+  users.groups."sync-cert".members = [ "nginx" "syncplay" ];
+  security.acme = { certs."sync.kittywit.ch" = { group = "sync-cert"; }; };
+
+  services.nginx.virtualHosts."sync.kittywit.ch" = {
+    enableACME = true;
+    forceSSL = true;
+  };
+
+  deploy.tf.dns.records.kittywitch_sync = {
+    tld = "kittywit.ch.";
+    domain = "sync";
+    cname.target = "athame.kittywit.ch.";
+  };
+
   services.syncplay = {
     enable = true;
     user = "syncplay";
-    group = "kittywit-ch";
+    group = "sync-cert";
     certDir = "/var/lib/acme/sync.kittywit.ch/";
   };
 
-  security.acme.certs."sync.kittywit.ch".postRun = ''
-    cp key.pem privkey.pem
-  '';
+  security.acme.certs."sync.kittywit.ch".postRun = "cp key.pem privkey.pem\nchown acme:voice-cert privkey.pem";
 }
