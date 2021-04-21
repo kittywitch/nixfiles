184e3575b5f6459b1dd10fd76ae57b0c82b7bd1f
kat witch <kat@kittywit.ch>
2021-04-21 22:44:44 +0100

    profiles/gui: MPV additions

--- profiles/gui/home/mpv.nix
+++ profiles/gui/home/mpv.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, witch, ... }:
 
 {
   programs.mpv = {
@@ -8,9 +8,40 @@
       profile = "gpu-hq";
       gpu-context = "wayland";
       vo = "gpu";
+      volume-max = 200;
+      keep-open = true;
+      opengl-waitvsync = true;
       hwdec = "auto";
       demuxer-max-bytes = "2000MiB";
       demuxer-max-back-bytes = "250MiB";
     };
   };
+
+    programs.syncplay = {
+      enable = false;
+      username = "kat";
+      defaultRoom = "lounge";
+      server = {
+        host = "sync.kittywit.ch";
+        password = witch.secrets.hosts.athame.syncplay.password;
+      };
+      # gui = false;
+      trustedDomains = [ "youtube.com" "youtu.be" "twitch.tv" "soundcloud.com" ];
+      config = {
+        client_settings = {
+          autoplayrequiresamefiles = false;
+          readyatstart = true;
+          pauseonleave = false;
+          rewindondesync = false;
+          rewindthreshold = 6.0;
+          fastforwardthreshold = 6.0;
+          unpauseaction = "Always";
+        };
+        gui = {
+          #autosavejoinstolist = false;
+          showdurationnotification = false;
+          chatoutputrelativefontsize = config.lib.gui.fontSize 13.0;
+        };
+      };
+    };
 }
