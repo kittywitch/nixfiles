4893bbaca7ea21c5701462dfe82c52e63a732152
kat witch <kat@kittywit.ch>
2021-04-23 00:18:12 +0100

    modules/home: Add nixexprs syncplay module

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -6,6 +6,7 @@
     ./vim.nix
     (sources.tf-nix + "/modules/home/secrets.nix")
     (import (sources.arc-nixexprs + "/modules")).home-manager.base16
+    (import (sources.arc-nixexprs + "/modules")).home-manager.syncplay
     #    (sources.arc-nixexprs + "/modules/home/base16-shell.nix")
     (sources.arc-nixexprs + "/modules/home/weechat.nix")
   ];
