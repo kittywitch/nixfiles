687cb5beed17de0ff4bcd05a312962ed6ad1d80c
kat witch <kat@kittywit.ch>
2021-04-25 18:52:10 +0100

    hosts/samhain: nixdirfmt result

--- hosts/samhain/home/sway.nix
+++ hosts/samhain/home/sway.nix
@@ -1,5 +1,4 @@
-{ config, pkgs, ... }: { 
-  wayland.windowManager.sway.config.startup = [
-    { command = "${pkgs.ckb-next}/bin/ckb-next -b"; }
-  ];
+{ config, pkgs, ... }: {
+  wayland.windowManager.sway.config.startup =
+    [{ command = "${pkgs.ckb-next}/bin/ckb-next -b"; }];
 }
