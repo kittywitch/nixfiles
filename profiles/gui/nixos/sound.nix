f659dd5ed87352c1bb2d614f7881003e3272ed65
kat witch <kat@kittywit.ch>
2021-04-26 16:49:11 +0100

    profiles/gui: All systems moved to pipewire

--- profiles/gui/nixos/sound.nix
+++ profiles/gui/nixos/sound.nix
@@ -7,8 +7,11 @@
       defaults.pcm.rate_converter "speexrate_best"
     '';
   };
+
+  /*
   hardware.pulseaudio = {
     enable = true;
+    extraConfig = "unload-module module-role-cork";
     daemon.config = {
       exit-idle-time = 5;
       resample-method = "speex-float-5";
@@ -19,5 +22,16 @@
       alternate-sample-rate = 44100;
       default-sample-channels = 2;
     };
+  };
+  */
+
+  security.rtkit.enable = true;
+
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+    alsa.support32Bit = true;
+    jack.enable = true;
+    alsa.enable = true;
   };
 }
