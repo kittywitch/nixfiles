60106e53afdd50e20a7839643f829ce9a9d5be6b
kat witch <kat@kittywit.ch>
2021-04-28 04:23:52 +0100

    profiles/gui: More moving into private for build

--- profiles/gui/home/email.nix
+++ profiles/gui/home/email.nix
@@ -7,24 +7,6 @@
   };
   programs.mbsync.enable = true;
   programs.msmtp.enable = true;
-  accounts.email = {
-    maildirBasePath = "${config.home.homeDirectory}/mail";
-    accounts.kat = {
-      address = "kat@kittywit.ch";
-      primary = true;
-      realName = "kat witch";
-      userName = "kat@kittywit.ch";
-      msmtp.enable = true;
-      mbsync.enable = true;
-      mbsync.create = "maildir";
-      notmuch.enable = true;
-      imap.host = "athame.kittywit.ch";
-      smtp.host = "athame.kittywit.ch";
-      gpg = {
-        signByDefault = true;
-        key = "01F50A29D4AA91175A11BDB17248991EFA8EFBEE";
-      };
-    };
-  };
+  accounts.email = { maildirBasePath = "${config.home.homeDirectory}/mail"; };
   programs.vim.plugins = [ pkgs.arc.pkgs.vimPlugins.notmuch-vim ];
 }
