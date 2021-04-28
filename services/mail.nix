46d930b184d51c8977edde6544c0a59b30caf7af
kat witch <kat@kittywit.ch>
2021-04-28 11:10:39 +0100

    services/mail: Private move

--- services/mail.nix
+++ services/mail.nix
@@ -86,28 +86,6 @@ with lib;
     enable = true;
     fqdn = "athame.kittywit.ch";
     domains = [ "kittywit.ch" "dork.dev" ];
-
-    # A list of all login accounts. To create the password hashes, use
-    # nix run nixpkgs.apacheHttpd -c htpasswd -nbB "" "super secret password" | cut -d: -f2
-    loginAccounts = {
-      "kat@kittywit.ch" = {
-        hashedPasswordFile = config.secrets.files.kat_mail_hash.path;
-
-        aliases = [ "postmaster@kittywit.ch" ];
-
-        # Make this user the catchAll address for domains kittywit.ch and
-        # example2.com
-        catchAll = [ "kittywit.ch" "dork.dev" ];
-      };
-    };
-
-    # Extra virtual aliases. These are email addresses that are forwarded to
-    # loginAccounts addresses.
-    extraVirtualAliases = {
-      # address = forward address;
-      "abuse@kittywit.ch" = "kat@kittywit.ch";
-    };
-
     # Use Let's Encrypt certificates. Note that this needs to set up a stripped
     # down nginx and opens port 80.
     certificateScheme = 3;
