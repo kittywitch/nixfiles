b179cfe01b0323d28fb086599b934515f77d84e0
kat witch <kat@kittywit.ch>
2021-05-03 16:54:13 +0100

    services/zfs: Auto-snapshot setup

--- services/zfs.nix
+++ services/zfs.nix
@@ -3,6 +3,12 @@
 {
   services.zfs = {
     autoScrub.enable = true;
-    autoSnapshot.enable = true;
+    autoSnapshot = {
+      enable = true;
+      frequent = 1;
+      daily = 7;
+      weekly = 1;
+      monthly = 1;
+    };
   };
 }
