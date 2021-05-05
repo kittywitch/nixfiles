e0764a44a43bbdc05500ac9ba7b2437f1ed5c608
kat witch <kat@kittywit.ch>
2021-05-05 15:03:55 +0100

    profiles/gui: PipeWire low latency for base service and pulse

--- profiles/gui/nixos/sound.nix
+++ profiles/gui/nixos/sound.nix
@@ -28,6 +28,27 @@
 
   services.pipewire = {
     enable = true;
+    config = {
+      pipewire = {
+        "context.properties" = {
+          "log.level" = 2;
+          "default.clock.min-quantum" =
+            32; # default; going lower may cause crackles and distorted audio
+        };
+        pipewire-pulse = {
+          "context.modules" = [{
+            name = "libpipewire-module-protocol-pulse";
+            args = {
+              "pulse.min.quantum" = 32; # controls minimum playback quant
+              "pulse.min.req" = 32; # controls minimum recording quant
+              "pulse.min.frag" = 32; # controls minimum fragment size
+              "server.address" =
+                [ "unix:native" ]; # the default address of the server
+            };
+          }];
+        };
+      };
+    };
     pulse.enable = true;
     alsa.support32Bit = true;
     jack.enable = true;
