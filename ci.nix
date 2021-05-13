3a5997cac000e1a5d3276ad8e3df4e50c6348c2e
kat witch <kat@kittywit.ch>
2021-05-13 18:32:46 +0100

    ci: moved to tasks

--- ci.nix
+++ ci.nix
@@ -1,13 +1,22 @@
-{ lib, ... }: with lib; {
+{ lib, channels, ... }: with lib; {
   name = "nixfiles";
   ci.gh-actions.enable = true;
   ci.gh-actions.export = true;
-  gh-actions.jobs.ci.step = let 
-    hostnames = [ "samhain" "yule" "athame"];
+
+  tasks = let hostnames = [ "samhain" "yule" "athame" ];
   in mapAttrs' (k: nameValuePair "host-${k}") (genAttrs hostnames (host: {
-    name = "build host/${host}";
-    run = "nix build -Lf . hosts.${host}.config.system.build.toplevel --show-trace";
-  }));
+      inputs = with channels.cipkgs; ci.command {
+        name = "build-${host}";
+        displayName = "build hosts/${host}";
+        nativeBuildInputs = [ nix ];
+        command = ''
+          nix build -Lf . hosts.${host}.config.system.build.toplevel --show-trace --no-link
+          nix-collect-garbage
+        '';
+        impure = true;
+      };
+    }));
+
   ci.gh-actions.checkoutOptions.submodules = false;
   cache.cachix.arc = {
     enable = true;
