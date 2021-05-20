cdff6abdc426a8739e9953a1efdcd8e6da66d027
kat witch <kat@kittywit.ch>
2021-05-20 02:16:31 +0100

    services/matrix: Taking some of arc's synapse config

--- services/matrix.nix
+++ services/matrix.nix
@@ -1,4 +1,6 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
+
+with lib;
 
 {
   environment.systemPackages = [ pkgs.mx-puppet-discord pkgs.mautrix-whatsapp ];
@@ -13,7 +15,6 @@
 
   services.matrix-synapse = {
     enable = true;
-    package = pkgs.unstable.matrix-synapse;
     max_upload_size = "512M";
     server_name = "kittywit.ch";
     app_service_config_files = [
@@ -21,6 +22,17 @@
       "/var/lib/matrix-synapse/discord-registration.yaml"
       "/var/lib/matrix-synapse/whatsapp-registration.yaml"
     ];
+    rc_messages_per_second = mkDefault "0.1";
+    rc_message_burst_count = mkDefault "25.0";
+    url_preview_enabled = mkDefault true;
+    enable_registration = mkDefault false;
+    enable_metrics = mkDefault false;
+    report_stats = mkDefault false;
+    dynamic_thumbnails = mkDefault true;
+    allow_guest_access = mkDefault true;
+    extraConfig = ''
+      suppress_key_server_warning: true
+    '';
     listeners = [{
       port = 8008;
       bind_address = "::1";
