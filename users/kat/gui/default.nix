e6b76a69ffa7fd40a90f7e9ae4f56851a27a691d
kat witch <kat@kittywit.ch>
2021-06-20 03:12:12 +0100

    users/kat/gui: Add ranger

--- users/kat/gui/default.nix
+++ users/kat/gui/default.nix
@@ -3,7 +3,7 @@
 {
   deploy.profile.gui = true;
 
-  imports = [ ./firefox ./kitty.nix ./packages.nix ./gtk.nix ./wezterm.nix ./foot.nix ./xdg.nix ];
+  imports = [ ./firefox ./kitty.nix ./packages.nix ./gtk.nix ./wezterm.nix ./foot.nix ./xdg.nix ./ranger.nix ];
 
   fonts.fontconfig.enable = true;
 }
