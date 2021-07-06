54b03946a5a2c2ae09b7fc2df281e0e39f20863a
kat witch <kat@kittywit.ch>
2021-07-06 01:33:31 +0100

    Cleanup in modules folders.

--- config/modules/home/default.nix
+++ config/modules/home/default.nix
@@ -4,7 +4,7 @@
   disabledModules = [ "programs/vim.nix" ];
   imports = with (import (sources.nixexprs + "/modules")).home-manager; [ base16 syncplay konawall i3gopher weechat shell ] ++ [
     ./vim.nix
-    ./deploy-tf
+    ./deploy-tf.nix
     (sources.tf-nix + "/modules/home/secrets.nix")
   ];
 }
