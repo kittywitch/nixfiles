f9b18c71220a15f8b8f0fca569f1e2002a2c2196
kat witch <kat@kittywit.ch>
2021-07-15 02:32:53 +0100

    Optimise store setting

--- config/profiles/common/nix.nix
+++ config/profiles/common/nix.nix
@@ -14,6 +14,7 @@
     binaryCaches = [ "https://arc.cachix.org" "https://kittywitch.cachix.org" ];
     binaryCachePublicKeys =
       [ "arc.cachix.org-1:DZmhclLkB6UO0rc0rBzNpwFbbaeLfyn+fYccuAy7YVY=" "kittywitch.cachix.org-1:KIzX/G5cuPw5WgrXad6UnrRZ8UDr7jhXzRTK/lmqyK0=" ];
+    autoOptimiseStore = true;
     gc.automatic = lib.mkDefault true;
     gc.options = lib.mkDefault "--delete-older-than 1w";
     trustedUsers = [ "root" "@wheel" ];
