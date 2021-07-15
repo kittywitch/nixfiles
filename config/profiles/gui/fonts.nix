b939799bdcf2565fb38672ed3d51141f7b1444cb
kat witch <kat@kittywit.ch>
2021-07-15 02:33:22 +0100

    Attempted fonts fix

--- config/profiles/gui/fonts.nix
+++ config/profiles/gui/fonts.nix
@@ -1,18 +1,23 @@
 { config, pkgs, lib, ... }:
 
 {
-  fonts.fontconfig = {
-    enable = true;
-    defaultFonts = {
-      emoji = [
-        "Twitter Color Emoji"
-      ];
+  fonts = {
+    enableDefaultFonts = true;
+    fontDir.enable = true;
+    fontconfig = {
+      enable = true;
+      defaultFonts = {
+        emoji = [
+          "Twitter Color Emoji"
+        ];
+      };
     };
   };
+
   fonts.fonts = with pkgs; [
-    font-awesome
-    nerdfonts
-    emacs-all-the-icons-fonts
-    twitter-color-emoji
+      font-awesome
+      nerdfonts
+      emacs-all-the-icons-fonts
+      twitter-color-emoji
   ];
 }
