ad41bd4fbab8fd1a82b5b85c8eee02e39b73ab62
kat witch <kat@kittywit.ch>
2021-08-05 21:48:44 +0100

    duplicate network config for samhain/ms-7b86

--- config/profiles/hardware/ms-7b86/default.nix
+++ config/profiles/hardware/ms-7b86/default.nix
@@ -11,23 +11,4 @@ with lib;
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usbhid" "sd_mod" ];
   boot.kernelModules = [ "nct6775" ];
-
-  systemd.network = {
-    networks.enp34s0 = {
-      matchConfig.Name = "enp34s0";
-      bridge = singleton "br";
-    };
-    networks.br = {
-      matchConfig.Name = "br";
-      address = [ "192.168.1.135/24" ];
-      gateway = [ "192.168.1.254" ];
-    };
-    netdevs.br = {
-      netdevConfig = {
-        Name = "br";
-        Kind = "bridge";
-        MACAddress = "00:d8:61:c7:f4:9d";
-      };
-    };
-  };
 }
