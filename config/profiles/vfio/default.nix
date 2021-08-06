ba6dfbd9819304ae54e14ca8ab04e8f1a6321fe9
kat witch <kat@kittywit.ch>
2021-08-06 17:36:19 +0100

    Move samhain specific VFIO config out of profiles.vfio

--- config/profiles/vfio/default.nix
+++ config/profiles/vfio/default.nix
@@ -45,14 +45,9 @@ with lib;
 
   services.udev.extraRules = ''
     SUBSYSTEM=="i2c-dev", GROUP="vfio", MODE="0660"
-    SUBSYSTEM=="usb", ACTION=="add", ATTRS{idVendor}=="1532", ATTRS{idProduct}=="0067", GROUP="vfio"
     SUBSYSTEM=="misc", KERNEL=="uinput", OPTIONS+="static_node=uinput", MODE="0660", GROUP="uinput"
     SUBSYSTEM=="vfio", OWNER="root", GROUP="vfio"
-    SUBSYSTEM=="block", ACTION=="add", ATTRS{model}=="HFS256G32TNF-N3A", ATTRS{wwid}=="t10.ATA     HFS256G32TNF-N3A0A                      MJ8BN15091150BM1Z   ", OWNER="kat"
   '';
-
-  # TODO: Replace this drive forward with one half of the 1.82TiB drive.
-  # SUBSYSTEM=="block", ACTION=="add", ATTR{partition}=="2", ATTR{size}=="1953503232", ATTRS{wwid}=="naa.5000039fe6e8614e", OWNER="kat"
 
   security.pam.loginLimits = [{
     domain = "@vfio";
