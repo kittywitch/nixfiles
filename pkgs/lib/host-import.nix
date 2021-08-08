6c4b775961dfa2d329ea5c13e6bc589445dc217d
kat witch <kat@kittywit.ch>
2021-08-08 02:28:15 +0100

    weh,,

--- pkgs/lib/host-import.nix
+++ pkgs/lib/host-import.nix
@@ -1,7 +1,7 @@
 { lib }: { hostName, profiles }: with lib; filter builtins.pathExists [
   (../../config/hosts + "/${hostName}/nixos.nix")
   (../../config/trusted/hosts + "/${hostName}/nixos.nix")
-] ++ (if builtins.isList profiles.base.imports then profiles.base.imports
+] ++ (if builtins.isAttrs profiles.base then profiles.base.imports
 else singleton profiles.base) ++ singleton {
   home-manager.users.kat = {
     imports = filter builtins.pathExists [
