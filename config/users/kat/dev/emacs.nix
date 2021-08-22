4602d4e1f98a076c5a6dcaf2b7d535f134798c22
kat witch <kat@kittywit.ch>
2021-08-22 05:41:03 +0100

    emacs: move to emacsPgtkGcc properly

--- config/users/kat/dev/emacs.nix
+++ config/users/kat/dev/emacs.nix
@@ -1,7 +1,7 @@
 { config, lib, pkgs, sources, ... }:
 
 let
-  emacs = pkgs.callPackage sources.nix-doom-emacs {
+  doom-emacs = pkgs.callPackage sources.nix-doom-emacs {
     doomPrivateDir = "${./doom.d}";
     emacsPackages = pkgs.emacsPackagesFor pkgs.emacsPgtkGcc;
     bundledPackages = false;
@@ -9,12 +9,15 @@ let
       magit-delta = super.magit-delta.overrideAttrs (esuper: {
         buildInputs = esuper.buildInputs ++ [ pkgs.git ];
       });
+      straight = self.straightBuild {
+        pname = "straight";
+      };
     };
   };
 in {
-  home.packages = [ emacs pkgs.sqlite ];
+  home.packages = [ doom-emacs pkgs.sqlite ];
 
-  home.file.".emacs.d/init.el".text = ''
+   home.file.".emacs.d/init.el".text = ''
     (load "default.el")
     (load-theme 'base16-${lib.elemAt (lib.splitString "." config.base16.alias.default) 1} t)
   '';
