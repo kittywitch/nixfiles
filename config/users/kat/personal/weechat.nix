671dd71d6ee3ff5b2c52e6f432a49df4cd8fa1e0
kat witch <kat@kittywit.ch>
2021-08-29 01:33:32 +0100

    weh

--- config/users/kat/personal/weechat.nix
+++ config/users/kat/personal/weechat.nix
@@ -1,7 +1,7 @@
 { config, nixos, pkgs, lib, ... }:
 
 {
-  home.file = let
+  home.file = mkIf config.deploy.profile.trusted (let
     bitw = pkgs.writeShellScriptBin "bitw" ''${pkgs.rbw-bitw}/bin/bitw -p gpg://${config.kw.repoSecrets.bitw.source} "$@"'';
   in { ".local/share/weechat/sec.conf".text = ''
       #
@@ -26,7 +26,7 @@
       znc = "552E98A5111B986C1003CF86C67DF2AF4B3FDE88E5762FC01EB4A00E31B8363ABFCBBE7A702CB72C298F61D4005D1C5AABB30602BBFCE9E4013CBE88D3D3DB66B18C551743D7816C4F0C9DA44B83DB5807BBB02A48B66D"
       matrix = "CC989DF79CDAECC1CE32F10FA9B42B6AE9FA63B41C0B3FCCCD4A309AB798CDEE695E0B4A2E0C975C6364927C76D4FEB25BC84C7AF8989DC418A205A5D62E9330E142E4F11AB59E0720867915DEEFCA70E80102C639D35B"
     '';
-  };
+  });
 
   programs.weechat = {
     enable = true;
