6cdcde51cf28b8c448e41a14ed1b0108fadbf018
kat witch <kat@kittywit.ch>
2021-08-30 02:05:29 +0100

    Remove broken ass known-hosts autocomplete

--- config/users/kat/base/shell.nix
+++ config/users/kat/base/shell.nix
@@ -63,9 +63,6 @@ in
         if [[ -r ~/.ssh/config ]]; then
           h=($h ''${''${''${(@M)''${(f)"$(cat ~/.ssh/config)"}:#Host *}#Host }:#*[*?]*})
         fi
-        if [[ -r ~/.ssh/known_hosts ]]; then
-          h=($h ''${''${''${(f)"$(cat ~/.ssh/known_hosts{,2} || true)"}%%\ *}%%,*}) 2>/dev/null
-        fi
         if [[ $#h -gt 0 ]]; then
           zstyle ':completion:*:ssh:*' hosts $h
           zstyle ':completion:*:slogin:*' hosts $h
