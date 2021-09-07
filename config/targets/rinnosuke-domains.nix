0c17f85fc808f841edba3b42db8abe82e170c696
kat witch <kat@kittywit.ch>
2021-09-07 05:05:17 +0100

    network: Fix domain creation for TF provisioned hosts.

--- config/targets/rinnosuke-domains.nix
+++ config/targets/rinnosuke-domains.nix
@@ -14,6 +14,21 @@ let rinnosuke = config.network.nodes.rinnosuke; in
         domain = rinnosuke.networking.hostName;
         aaaa.address = rinnosuke.network.addresses.public.nixos.ipv6.address;
       };
+      node_wireguard_rinnosuke_v4 = {
+        inherit (rinnosuke.network.dns) zone;
+        domain = rinnosuke.network.addresses.wireguard.subdomain;
+        a.address = rinnosuke.network.addresses.wireguard.nixos.ipv4.address;
+      };
+      node_wireguard_rinnosuke_v6 = {
+        inherit (rinnosuke.network.dns) zone;
+        domain = rinnosuke.network.addresses.wireguard.subdomain;
+        aaaa.address = rinnosuke.network.addresses.wireguard.nixos.ipv6.address;
+      };
+      node_yggdrasil_rinnosuke_v6 = {
+        inherit (rinnosuke.network.dns) zone;
+        domain = rinnosuke.network.addresses.yggdrasil.subdomain;
+        aaaa.address = rinnosuke.network.addresses.yggdrasil.nixos.ipv6.address;
+      };
     };
   };
 }
