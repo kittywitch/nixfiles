9bca2d6940c5cecf5f376d99c4b2d9373d58a35f
kat witch <kat@kittywit.ch>
2021-09-09 20:46:41 +0100

    profiles/base: further wheel allows

--- config/hosts/daiyousei.nix
+++ config/hosts/daiyousei.nix
@@ -22,13 +22,6 @@
     };
   };
 
-  network = {
-    yggdrasil = {
-      enable = true;
-      pubkey = "89771aa2f15fce6bbc3548f95be360cf59657d299837b10adf53944b54e8f121";
-    };
-  };
-
   services.nginx.virtualHosts =
     let
       splashy = pkgs.host-splash-site config.networking.hostName;
--- config/profiles/base/access.nix
+++ config/profiles/base/access.nix
@@ -3,11 +3,20 @@
 {
   security.sudo.wheelNeedsPassword = lib.mkForce false;
 
+  security.polkit.extraConfig = ''
+    polkit.addRule(function(action, subject) {
+      if (subject.isInGroup("wheel")) {
+        return polkit.Result.YES;
+      }
+    });
+  '';
+
   imports = with meta; [
     users.kat.base
   ];
 
   users.users.root = {
+    shell = pkgs.zsh;
     hashedPassword =
       "$6$i28yOXoo$/WokLdKds5ZHtJHcuyGrH2WaDQQk/2Pj0xRGLgS8UcmY2oMv3fw2j/85PRpsJJwCB2GBRYRK5LlvdTleHd3mB.";
     openssh.authorizedKeys.keys = with pkgs.lib;
