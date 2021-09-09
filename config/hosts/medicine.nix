dcb96480c621ea21f39f65ac0bc2c73d4ac639f4
kat witch <kat@kittywit.ch>
2021-09-09 03:19:58 +0100

    hosts/medicine: Init

---
+++ config/hosts/medicine.nix
@@ -0,0 +1,30 @@
+{ meta, config, pkgs, modulesPath, lib, ... }: with lib; {
+  options.home-manager.users = let
+    userNMExtend = { config, nixos, ... }: {
+      services.network-manager-applet.enable = true;
+    };
+    userBase16Extend = { config, nixos, ... }: {
+      base16.alias.default = "atelier.atelier-cave-light";
+    };
+  in mkOption {
+    type = types.attrsOf (types.submoduleWith {
+      modules = [ userNMExtend userBase16Extend ];
+    });
+  };
+
+  imports = with meta; [
+    profiles.laptop
+    profiles.gui
+    users.kat.guiFull
+    (modulesPath + "/installer/cd-dvd/installation-cd-base.nix")
+  ];
+
+  config = {
+    networking = {
+      useDHCP = true;
+      networkmanager.enable = true;
+    };
+
+    system.stateVersion = "21.11";
+  };
+}
