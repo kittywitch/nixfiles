882c86f368990d46dc25cf897f4d2a70ec5bd558
Youko <git@chakat.space>
2021-10-21 12:46:12 -0600

    fix loop on marisa?

--- config/profiles/network.nix
+++ config/profiles/network.nix
@@ -27,7 +27,7 @@
 
     networking.firewall.extraCommands = ''
     ip6tables -A INPUT -p 89 -i wgmesh-+ -j ACCEPT
-    ip route replace to 10.42.68.0/24 via ${nodes.marisa.network.addresses.yggdrasil.nixos.ipv4.address}
+    ${if config.networking.hostName != "marisa" then "ip route replace to 10.42.68.0/24 via ${nodes.marisa.network.addresses.yggdrasil.nixos.ipv4.address}" else ""}
     '';
     networking.nftables.extraInput = ''
       meta l4proto 89 iifname wgmesh-* accept
