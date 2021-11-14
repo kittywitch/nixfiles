cbcf9871807a306d5849173738358edf762cc189
kat witch <kat@kittywit.ch>
2021-11-14 21:00:26 +0000

    Bluetooth and Sound work, reverse latency changes

--- config/profiles/hardware/networkmanager.nix
+++ config/profiles/hardware/networkmanager.nix
@@ -15,8 +15,35 @@
     };
   };
   config = {
-    hardware.bluetooth.enable = true;
+    hardware.bluetooth = {
+      enable = true;
+      settings = {
+        General = {
+          Enable = "Source,Sink,Media,Socket";
+        };
+      };
+    };
     services.blueman.enable = true;
+    services.pipewire.media-session.config.bluez-monitor = {
+      properties = { };
+      rules = [
+        {
+          actions = {
+            update-props = {
+              "bluez5.a2dp-source-role" = "input";
+              "bluez5.auto-connect"  = [ "hfp_hf" "hsp_hs" "a2dp_sink" "a2dp_source" "hsp_ag" "hfp_ag" ];
+            };
+          };
+          matches = [ { "device.name" = "~bluez_card.*"; } ];
+        }
+        {
+          actions = {
+            update-props = { "node.pause-on-idle" = false; };
+          };
+          matches = [ { "node.name" = "~bluez_input.*"; } { "node.name" = "~bluez_output.*"; } ];
+        }
+      ];
+    };
     networking = {
       networkmanager = {
         enable = true;
