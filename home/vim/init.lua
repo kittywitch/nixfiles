3ed673d0a1ab2d7b7489e812c5262adaa1d65d29
kat <kat@inskip.me>
2022-07-17 16:41:48 -0700

    fix: set up hop on nvim

--- home/vim/init.lua
+++ home/vim/init.lua
@@ -165,6 +165,8 @@ api.nvim_set_keymap('n', '<leader>fb', '<cmd>Telescope buffers<cr>', { noremap =
 api.nvim_set_keymap('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', { noremap = true, silent = true })
 
 -- hop
+require'hop'.setup()
+
 vim.api.nvim_set_keymap('', 'f', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>", {})
 vim.api.nvim_set_keymap('', 'F', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>", {})
 vim.api.nvim_set_keymap('', 't', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })<cr>", {})
