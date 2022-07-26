00ccc1e45872daf7d52de555d33ecddfce6047b8
kat <kat@inskip.me>
2022-07-25 18:14:05 -0700

    refactor(home/vim): vim.set.keymap for telescope

--- home/vim/init.lua
+++ home/vim/init.lua
@@ -178,10 +178,23 @@ require('neorg').setup {
 }
 
 -- telescope
-api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope find_files<cr>', { noremap = true, silent = true })
-api.nvim_set_keymap('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', { noremap = true, silent = true })
-api.nvim_set_keymap('n', '<leader>fb', '<cmd>Telescope buffers<cr>', { noremap = true, silent = true })
-api.nvim_set_keymap('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', { noremap = true, silent = true })
+local telescope = require('telescope.builtin')
+
+vim.keymap.set("n", "<leader>ff", function()
+	telescope.find_files()
+end, { silent = true })
+
+vim.keymap.set("n", "<leader>fg", function()
+	telescope.live_grep()
+end, { silent = true })
+
+vim.keymap.set("n", "<leader>fb", function()
+	telescope.buffers()
+end, { silent = true })
+
+vim.keymap.set("n", "<leader>fh", function()
+	telescope.help_tags()
+end, { silent = true })
 
 -- tresitter
 require'nvim-treesitter.configs'.setup {
