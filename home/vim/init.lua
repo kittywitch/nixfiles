d33717e3dc931e7c8d9b002548918b705f14e2fa
kat <kat@inskip.me>
2022-07-25 18:16:24 -0700

    style(home/vim): additional newline near a setup

--- home/vim/init.lua
+++ home/vim/init.lua
@@ -154,7 +154,8 @@ cmp.setup({
 })
 
 -- lspconfig
-require('lspconfig').terraformls.setup{}
+require('lspconfig').terraformls.setup()
+
 api.nvim_create_autocmd('BufWritePre', {
 	pattern = '*.tf',
 	command = 'lua vim.lsp.buf.formatting_sync()'
