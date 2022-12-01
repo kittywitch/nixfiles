d273b25fb55a9584675f118c94904db1db7f3ed6
Kat Inskip <kat@inskip.me>
2022-12-01 22:12:15 +0100

    feat: specialArgs, aarch64-darwin

--- default.nix
+++ default.nix
@@ -16,13 +16,21 @@
 in {
   inherit tree;
   nixosConfigurations = mapAttrs (_: path: nixpkgs.lib.nixosSystem {
+    specialArgs = {
+      inherit inputs tree;
+      machine = name;
+    };
     system = "x86_64-linux";
     modules = [
       path
     ];
     } ) tree.nixos.systems;
   darwinConfigurations = mapAttrs (_: path: darwin.lib.darwinSystem {
-    system = "x86_64-linux";
+    specialArgs = {
+      inherit inputs tree;
+      machine = name;
+    };
+    system = "aarch64-darwin";
     modules = [
       path
     ];
