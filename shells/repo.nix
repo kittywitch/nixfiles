f9d8514202b3e740dde9a5e1b8458773380753ce
Kat Inskip <kat@inskip.me>
2023-01-25 09:00:12 -0800

    ops: remove python pulumi

--- shells/repo.nix
+++ shells/repo.nix
@@ -7,25 +7,11 @@
 with pkgs; let
   repo = import ../outputs.nix { inherit inputs; };
   inherit (std) set list;
-  python = "python39";
-  mergeEnvs = envs:
-    pkgs.mkShell (list.foldl'
-      (a: v: {
-        buildInputs = a.buildInputs ++ v.buildInputs;
-        nativeBuildInputs = a.nativeBuildInputs ++ v.nativeBuildInputs;
-      })
-      (pkgs.mkShell { })
-      envs);
-  requirements = builtins.readFile ../requirements.txt;
-  mach-nix-wrapper = import inputs.mach-nix { inherit pkgs python; };
-  pythonShell = mach-nix-wrapper.mkPythonShell { inherit requirements; };
   repoShell = mkShell {
     nativeBuildInputs =
       [
         go # Required for pulumi
         pulumi-bin # Infrastructure as code
-        python39Packages.pulumi # Pulumi for Python!
-        pulumiPackages.pulumi-language-python # Python!
         deadnix # dead-code scanner
         alejandra # code formatter
         statix # anti-pattern finder
@@ -35,4 +21,4 @@ with pkgs; let
       ''))
       repo.darwinConfigurations);
   };
-in mergeEnvs [ repoShell pythonShell ]
+in repoShell
