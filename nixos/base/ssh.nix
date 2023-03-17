5edafc266436f768d36c7357a3feaafff0cf71b0
arcnmx <git@git.arcn.mx>
2023-03-17 09:57:19 -0700

    flake update

--- nixos/base/ssh.nix
+++ nixos/base/ssh.nix
@@ -18,15 +18,15 @@ with lib;
   services.openssh = {
     enable = true;
     ports = lib.mkDefault [ 62954 ];
-    passwordAuthentication = false;
-    kbdInteractiveAuthentication = false;
-    permitRootLogin = lib.mkDefault "prohibit-password";
-    kexAlgorithms = [ "curve25519-sha256@libssh.org" ];
-    extraConfig = ''
-      PubkeyAcceptedAlgorithms +ssh-rsa
-      StreamLocalBindUnlink yes
-      LogLevel VERBOSE
-    '';
+    settings = {
+      PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
+      PermitRootLogin = lib.mkDefault "prohibit-password";
+      KexAlgorithms = [ "curve25519-sha256@libssh.org" ];
+      PubkeyAcceptedAlgorithms = "+ssh-rsa";
+      StreamLocalBindUnlink = "yes";
+      LogLevel = "VERBOSE";
+    };
   };
 
   programs.mosh.enable = true;
