7cd1c477274d630209fa2ea5aa4c6a5e6c0d3bf9
Kat Inskip <kat@inskip.me>
2023-04-29 14:23:13 -0700

    [CLOUDFLARE PAGES] Import inskip.me into terraform

---
+++ cloudflare-inskip-pages.tf
@@ -0,0 +1,28 @@
+resource "cloudflare_pages_project" "inskip_root" {
+  account_id = "0467b993b65d8fd4a53fe24ed2fbb2a1"
+  name = "inskip-root"
+  production_branch = "main"
+
+  source {
+    type = "github"
+    config {
+        owner = "kittywitch"
+        repo_name = "inskip.me"
+        production_branch = "main"
+        deployments_enabled = true
+        pr_comments_enabled = false
+        production_deployment_enabled = true
+    }
+  }
+  build_config {
+    build_command = "hugo"
+    destination_dir = "/public"
+    root_dir = "/"
+  }
+}
+
+resource "cloudflare_pages_domain" "inskip_root" {
+    account_id = "0467b993b65d8fd4a53fe24ed2fbb2a1"
+    project_name = "inskip-root"
+    domain = "inskip.me"
+}
