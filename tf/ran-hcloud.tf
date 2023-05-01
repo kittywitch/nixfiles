13a5da246a6b60ccc28dc8ee4252cae088aee55a
Kat Inskip <kat@inskip.me>
2023-04-30 17:32:51 -0700

    [RAN] IP unique resource name

--- tf/ran-hcloud.tf
+++ tf/ran-hcloud.tf
@@ -1,4 +1,4 @@
-resource "hcloud_primary_ip" "ipv4" {
+resource "hcloud_primary_ip" "ran_ipv4" {
 	auto_delete = false
 	name = "ran-v4"
 	datacenter = "hil-dc1"
@@ -7,7 +7,7 @@ resource "hcloud_primary_ip" "ipv4" {
 }
 
 
-resource "hcloud_primary_ip" "ipv6" {
+resource "hcloud_primary_ip" "ran_ipv6" {
 	auto_delete = false
 	name = "ran-v6"
 	datacenter = "hil-dc1"
@@ -23,9 +23,9 @@ resource "hcloud_server" "ran" {
     datacenter = "hil-dc1"
     public_net {
         ipv4_enabled = true
-        ipv4 = hcloud_primary_ip.ipv4.id
+        ipv4 = hcloud_primary_ip.ran_ipv4.id
         ipv6_enabled = true
-        ipv6 = hcloud_primary_ip.ipv6.id
+        ipv6 = hcloud_primary_ip.ran_ipv6.id
     }
 
     lifecycle {
