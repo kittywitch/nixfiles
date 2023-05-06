8e3a8256a37ecbdeac81badb248cf40debc52f85
Kat Inskip <kat@inskip.me>
2023-05-06 12:07:57 -0700

    [LOCAL PATH PROVISIONER] Use the helm chart.

---
+++ cluster/local-path-provisioner.tf
@@ -0,0 +1,11 @@
+resource "helm_release" "local_path_provisioner" {
+    name = "local-path-provisioner"
+    repository = "./lpp"
+    chart = "local-path-storage"
+    create_namespace = true
+    namespace = "local-path-storage"
+
+    timeout = var.helm_timeout
+    cleanup_on_fail = true
+    force_update = true
+}
