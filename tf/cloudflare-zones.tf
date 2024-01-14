77ac550bdc187c430d0ab63d1a24439d2a9f1aa2
Kat Inskip <kat@inskip.me>
2024-01-14 13:23:47 -0800

    Remove gensokyo.zone

--- tf/cloudflare-zones.tf
+++ tf/cloudflare-zones.tf
@@ -2,13 +2,11 @@ locals {
   account_id = "0467b993b65d8fd4a53fe24ed2fbb2a1"
   zones = {
     dork = "dork.dev"
-    gensokyo = "gensokyo.zone"
     inskip = "inskip.me"
     kittywitch = "kittywit.ch"
   }
   zone_ids = {
     dork = cloudflare_zone.dork_zone.id
-    gensokyo = cloudflare_zone.gensokyo_zone.id
     inskip = cloudflare_zone.inskip_zone.id
     kittywitch = cloudflare_zone.kittywitch_zone.id
   }
@@ -19,14 +17,6 @@ resource "cloudflare_zone" "dork_zone" {
   plan       = "free"
   type       = "full"
   zone       = local.zones.dork
-}
-
-resource "cloudflare_zone" "gensokyo_zone" {
-  account_id = local.account_id
-  paused     = false
-  plan       = "free"
-  type       = "full"
-  zone       = local.zones.gensokyo
 }
 
 resource "cloudflare_zone" "inskip_zone" {
--- tf/gensokyo-public.tf
+++
@@ -1,8 +0,0 @@
-resource "cloudflare_record" "public" {
-  name    = "public"
-  proxied = true
-  ttl     = 1
-  type    = "CNAME"
-  value   = "yukari.gensokyo.zone"
-  zone_id = local.zone_ids.gensokyo
-}
