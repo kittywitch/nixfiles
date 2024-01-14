773ed2803283749291b311de54f918053fbd45db
Kat Inskip <kat@inskip.me>
2024-01-14 13:24:19 -0800

    Remove gensokyo outputs

--- tf/outputs.tf
+++
@@ -1,7 +0,0 @@
-output  "apartment_cloudflare_tunnel_id" {
-    value = cloudflare_tunnel.gensokyo_apartment_tunnel.id
-}
-output  "apartment_cloudflare_tunnel_token" {
-    value = cloudflare_tunnel.gensokyo_apartment_tunnel.tunnel_token
-    sensitive = true
-}
