7f69011deb6b5828d46c27aaeb2bbad0b89a0742
Kat Inskip <kat@inskip.me>
2024-01-30 14:30:12 -0800

    feat(quest): fix sidequest

--- nixos/profiles/gaming/quest.nix
+++ nixos/profiles/gaming/quest.nix
@@ -2,7 +2,15 @@
   environment.systemPackages = with pkgs; [
     questpatcher
     sidequest
+    gsettings-desktop-schemas
   ];
   programs.adb.enable = true;
   users.users.kat.extraGroups = ["adbusers"];
+  services.udev.extraRules = ''
+    SUBSYSTEM=="usb",ATTR{idVendor}=="2833",ATTR{idProduct}=="0186",MODE="0660",GROUP="adbusers",TAG+="uaccess",SYMLINK+="android",SYMLINK+="android%n"
+  '';
+  home-manager.users.kat.xdg.systemDirs.data = [
+    "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}"
+    "${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}"
+  ];
 }
