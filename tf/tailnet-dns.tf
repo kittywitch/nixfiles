66902eb1593fdc2479c11627d04d5908ac2b65dd
Kat Inskip <kat@inskip.me>
2024-08-03 16:29:44 -0700

    feat: change value for domain to have devices prefix

--- tf/tailnet-dns.tf
+++ tf/tailnet-dns.tf
@@ -7,7 +7,7 @@ locals {
 
 resource "cloudflare_record" "tailscale_device_v4_record" {
   for_each = { for device_name, device in local.tailscale_devices : split(".", device.name)[0] => device.addresses[0] if device.user == "kat@gensokyo.zone" }
-  name    = each.key
+  name    = "${each.key}.devices"
   proxied = false
   ttl     = 3600
   type    = "A"
@@ -17,7 +17,7 @@ resource "cloudflare_record" "tailscale_device_v4_record" {
 
 resource "cloudflare_record" "tailscale_device_v6_record" {
   for_each = { for device_name, device in local.tailscale_devices : split(".", device.name)[0] => device.addresses[1] if device.user == "kat@gensokyo.zone" }
-  name    = each.key
+  name    = "${each.key}.devices"
   proxied = false
   ttl     = 3600
   type    = "AAAA"
